6a05dece5c1cc83c53544cc2c59396e39693c600
Andrei Krutoy <andrei.krutoy1997@gmail.com>
2021-11-07 11:34:24 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace project_less
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int myVar = 256;*/ 
+
+            Student student = new Student();
+            student._age = 5;
+
+            Calculate(student);
+
+            Console.WriteLine(student._age);
+            
+            Console.ReadLine();
+        }
+        static void Calculate(Student student){
+            student._age += 1;
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,7 @@
+namespace project_less
+{
+    public class Student
+    {
+        public int _age;
+    }
+}
